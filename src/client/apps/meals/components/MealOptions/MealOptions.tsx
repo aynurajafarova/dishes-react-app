1b76d91e667045cb8c3779cdbc84cbcdfb3a4e00
aynurajafarova <aynurafj@code.edu.az>
2022-04-07 14:40:21 +0400

    feat(meal-options): find meal by id from mealOptions array

--- src/client/apps/meals/components/MealOptions/MealOptions.tsx
+++ src/client/apps/meals/components/MealOptions/MealOptions.tsx
@@ -1,24 +1,38 @@
-import { FC } from "react";
+import { ChangeEvent, FC } from "react";
 
-import { mealOptions } from "../../../../shared/helpers/meals";
+import { IMealOption } from "../../../../shared/models/meal";
 
 import "./MealOptions.scss";
 
-const MealOptions: FC = () => {
+interface IProps {
+  mealOptions: IMealOption[];
+  findMeal: (id: string) => void;
+  setMealId: (id: string) => void;
+}
+
+const MealOptions: FC<IProps> = ({ mealOptions, findMeal, setMealId }) => {
   return (
-    <div className="meal-types center">
-      {mealOptions?.map(({ id, name, type, defaultChecked, title, img }) => {
-        return (
-          <label className="meal-types__item" htmlFor={id} key={id}>
-            <input {...{ type, name, id, defaultChecked }} />
-            <div className="meal-types__item__body center">
-              <img src={img} alt={title} />
-              <span>{title}</span>
-            </div>
-          </label>
-        );
-      })}
-    </div>
+    <>
+      <div className="meal-types center">
+        {mealOptions?.map(({ id, name, type, defaultChecked, title, img }) => {
+          return (
+            <label className="meal-types__item" htmlFor={id} key={id}>
+              <input
+                {...{ type, name, id, defaultChecked }}
+                onChange={(e: ChangeEvent<HTMLInputElement>) => {
+                  setMealId(e.target.id);
+                  findMeal(e.target.id);
+                }}
+              />
+              <div className="meal-types__item__body center">
+                <img src={img} alt={title} />
+                <span>{title}</span>
+              </div>
+            </label>
+          );
+        })}
+      </div>
+    </>
   );
 };
 
--- src/client/apps/meals/view/OrderMealPage.tsx
+++ src/client/apps/meals/view/OrderMealPage.tsx
@@ -1,17 +1,30 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 
 import OrderMealForm from "../components/OrderMealForm/OrderMealForm";
 import MealOptions from "../components/MealOptions/MealOptions";
 import Header from "../../../shared/components/Header/Header";
 import Container from "../../../shared/components/Container/Container";
+import { mealOptions } from "../../../shared/helpers/meals";
+import { IMealOption } from "../../../shared/models/meal";
 
 const OrderMealPage: FC = () => {
+  const [mealId, setMealId] = useState<string>(mealOptions[0]?.id);
+
+  const findMeal = (mealId: string) => {
+    const newArr: IMealOption[] = mealOptions.filter(
+      (item: IMealOption) => item.id === mealId
+    );
+    console.log(newArr);
+  };
+
+  console.log(mealId);
+
   return (
     <main>
       <Container>
         <Header className="center" title="Food order form" />
         <OrderMealForm />
-        <MealOptions />
+        <MealOptions {...{ mealOptions, findMeal, setMealId }} />
       </Container>
     </main>
   );
