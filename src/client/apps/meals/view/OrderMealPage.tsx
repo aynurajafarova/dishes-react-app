db89879f6db0353f42e7983e4c8086022731f194
aynurajafarova <aynurafj@code.edu.az>
2022-04-07 11:58:09 +0400

    feat(container): create Container component

--- src/client/apps/meals/view/OrderMealPage.tsx
+++ src/client/apps/meals/view/OrderMealPage.tsx
@@ -3,13 +3,16 @@ import { FC } from "react";
 import OrderMealForm from "../components/OrderMealForm/OrderMealForm";
 import MealOptions from "../components/MealOptions/MealOptions";
 import Header from "../../../shared/components/Header/Header";
+import Container from "../../../shared/components/Container/Container";
 
 const OrderMealPage: FC = () => {
   return (
     <main>
-      <Header title="Are you hungry?" />
-      <OrderMealForm />
-      <MealOptions />
+      <Container>
+        <Header className="center" title="Are you hungry?" />
+        <OrderMealForm />
+        <MealOptions />
+      </Container>
     </main>
   );
 };
--- src/client/shared/components/Header/Header.tsx
+++ src/client/shared/components/Header/Header.tsx
@@ -4,11 +4,12 @@ import "./Header.scss";
 
 interface IProps {
   title: string;
+  className?: string;
 }
 
-const Header: FC<IProps> = ({ title }) => {
+const Header: FC<IProps> = ({ title, className = "" }) => {
   return (
-    <header className="header center">
+    <header className={`header ${className}`}>
       <h1>{title}</h1>
     </header>
   );
